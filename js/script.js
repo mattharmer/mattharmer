d29127150e1eed170ec7482f23c9dd1bb5bbe675
Matt Harmer <harmer.matt@gmail.com>
2019-07-21 16:35:44 +1000

    js to bottom of body...

--- js/script.js
+++ js/script.js
@@ -3,17 +3,14 @@ var menu = document.getElementById(menu);
 var nav = document.getElementById(nav);
 var exit = document.getElementById(exit);
 
+menu.addeventListener('click', function(e) {
+nav.classList.toggle('hideMobile');
+alert("Hello! Alert!");
+e.preventDefault();
+});
 
-  menu.addeventListener('click', function(e) {
-  nav.classList.toggle('hideMobile');
-  alert("Hello! Alert!");
-  e.preventDefault();
-  });
-}
 
-if (exit) {
 exit.addeventListener('click', function(e) {
 nav.classList.add('hideMobile');
 e.preventDefault();
 });
-}
