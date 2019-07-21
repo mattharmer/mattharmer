8f31899ca6d9300b8d1ebf0f73690d198a9e901e
Matt Harmer <harmer.matt@gmail.com>
2019-07-22 09:44:49 +1000

    Mobile Menu js done.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
-
-var menu = document.getElementById(menu);
-var nav = document.getElementById(nav);
-var exit = document.getElementById(exit);
-
-menu.addeventListener('click', function(e) {
-nav.classList.toggle('hideMobile');
-alert("Hello! Alert!");
-e.preventDefault();
-});
+// Select the elements we want to show or hide
+var mobileBtn = document.querySelector('.mobile-btn');
+var menu = document.querySelector('.menu');
+var closeBtn = document.querySelector('.close');
+var overlay = document.querySelector('.overlay');
 
 
-exit.addeventListener('click', function(e) {
-nav.classList.add('hideMobile');
-e.preventDefault();
-});
+// When the user clicks on the hamburger icon the 'open' class is added
+// to both the menu and overlay elements making them visible and triggering the animation
+mobileBtn.addEventListener('click', function(){
+  menu.className += ' open';
+  overlay.className += ' open';
+})
+
+
+// When the close button is clicked the 'open' class is removed from
+// both the menu and overlay elements making them invisible
+closeBtn.addEventListener('click', function(){
+  menu.className = 'menu';
+  overlay.className = 'overlay';
+})
+
+// If a user clicks outside the menu on the overlay it will also trigger the event
+// to hide the menu and overlay elements
+window.addEventListener('click', function(event){
+  if(event.target === overlay){
+    menu.className = 'menu';
+    overlay.className = 'overlay';
+  }
+})
