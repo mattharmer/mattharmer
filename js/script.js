8025e7151e2c9cd09e487cb23ea0a09499467448
Matt Harmer <harmer.matt@gmail.com>
2019-07-21 16:25:22 +1000

    javascript menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+
 var menu = document.getElementById(menu);
 var nav = document.getElementById(nav);
 var exit = document.getElementById(exit);
+
+
+  menu.addeventListener('click', function(e) {
+  nav.classList.toggle('hideMobile');
+  alert("Hello! Alert!");
+  e.preventDefault();
+  });
+}
+
+if (exit) {
+exit.addeventListener('click', function(e) {
+nav.classList.add('hideMobile');
+e.preventDefault();
+});
+}
